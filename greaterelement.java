1aaadf1691477b5784b7754753c41c96e0f8e139
Balamurugan M <balamurugan20406@gmail.com>
2026-09-09 13:09:40 +0530

    Add files via upload

---
+++ greaterelement.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class greaterelement {
+public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n= sc.nextInt();
+    int arr[] = new int[n];
+    for(int i=0; i<n; i++){
+        arr[i]= sc.nextInt();
+
+    }
+    int max= arr[0];
+    System.out.print(max);
+    for(int i=0;i<n; i++){
+        if(arr[i]>max){
+           System.out.print(arr[i]+"");
+           max= arr[i];
+        }
+        
+    }
+}
+    
+}
